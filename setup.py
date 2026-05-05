0a1624d7bfad36e794b977fd04c5bfb2e2afdcdd
Vasanthivuppala <vasanthivuppala890@gmail.com>
2026-05-05 21:22:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup
+setup(
+    name="Medical_chatbot",
+    version="0.1.0",
+    author="Vasanthi",
+    author_email="vasanthivuppala890@gmail.com",
+    install_requires=[
+        # "langchain==0.3.26",
+        # "flask==3.1.1",
+        # "sentence-transformer==4.1.0",
+        # "pypdf==5.6.1",
+        # "python-dotenv==1.1.0",
+        # "langchain-pinecone==0.2.8",
+        # "langchain-openai==0.3.24",
+        # "langchain-community==0.3.26",
+    ]
+    )
